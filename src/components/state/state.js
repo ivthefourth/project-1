1bd8942e52b794a5b7e375eca352291c61264563
Howard Shaw <chip.shaw4@gmail.com>
2017-09-01 16:00:19 -0600

    started interests

--- src/components/state/state.js
+++ src/components/state/state.js
@@ -1,14 +1,106 @@
-import {interestList} from '../recreation/recreation';
+import {interestList as list} from '../recreation/recreation';
+
+class EventObject{
+   constructor(eventsArr){
+      let events = this.events = {};
+      eventsArr.forEach(function(e){
+         //this array will contain callback functions
+         events[e] = [];
+      });
+
+   }
+
+   //set event listener
+   on(event, callback){
+      if(this.events[event] == undefined){
+         throw new Error(`"${event}" event does not exist on ${this}`)
+      }
+      else if(typeof callback !== 'function'){
+         throw new Error(`Second argument to "${this}.on()" must be a function.`)
+      }
+      else{
+         this.events[event].push(callback);
+      }
+   }
+
+   //trigger event listeners for given event
+   emit(event){
+      if(this.events[event] == undefined){
+         throw new Error(`"${event}" event does not exist on ${this}`)
+      }
+      else{
+         let callbacks = this.events[event];
+         let e = this.makeEvent();
+         //execute all callbacks
+         callbacks.forEach(function(c){
+            c(e);
+         })
+      }
+   }
+
+   //provides event object for event listeners; should be overwritten by inheritor
+   makeEvent(){
+      console.warn(`No makeEvent method set on ${this}.`)
+   }
+}
 
 /*************\    
    Interests    
 \*************/
-class Interest{
-   ;
+class Interest extends EventObject{
+   constructor(interest){
+      super(['change']);
+      this.name = interest.ActivityName;
+      this.id = interest.ActivityID;
+      this.iconId = interest.Emoji
+
+      this.selected = false;
+
+      this.makeEvent = this.makeEvent.bind(this);
+   }
+   //toggles selected property
+   toggle(){
+      this.selected = !this.selected;
+      this.emit('change');
+   }
+   toString(){
+      return "Interest";
+   }
+   makeEvent(){
+      return {val: this.selected};
+   }
+}
+
+class Interests extends EventObject{
+   //list is list of interests, to be provided by recreation module 
+   constructor(list){
+      super(['change']);
+      this.all = list.map(function(i){
+         let interest = new Interest(i);
+         interest.on('change', this.emit.bind(this, 'change'));
+         return interest;
+      });
+
+      this.makeEvent = this.makeEvent.bind(this);
+   }
+   get selected(){
+      return this.all.filter();
+   }
+   toString(){
+      return "state.interests";
+   }
+   makeEvent(){
+      return {
+         val: {
+            all: this.all,
+            selected: this.selected
+         }
+      };
+   }
 }
 
 /*************\    
-    Route    
+     Route    
 \*************/
 class Location{
    ;
@@ -16,14 +108,82 @@ class Location{
 
 
 /*************\    
-      
+ Overall State
 \*************/
 class State {
    constructor(){
-      this.interestList = interestList;
+      this.interests = null;
+   }
+   setInterests(list){
+      this.interests = new Interests(list);
    }
 }
 
-const state = new State;
+const STATE = new State;
+STATE.setInterests(list);
 
-export default state;
+export default STATE;
+
+
+//State Diagram
+
+
+// state = {
+//    setInterests: function(){},
+//    INTERESTS: {
+//       all: [{
+//          name: 'string',
+//          id: 'number',
+//          iconId: 'string',
+//          selected: 'boolean',
+//          toggle: function(){},
+//          on: function(eventString, callback){},
+//          events: {
+//             change: [ function(e){}, function(e){} ],
+//          },
+//          emit: function(eventString);// trigger event listeners for given event
+//       }, 
+//       {...}, 
+//       {...}],
+//       //returns an array of only selected interests (use getter)
+//       selected: [{...}, {...}],
+//       on: function(eventString, callback){},
+//       events: {
+//          change: [ function(){}, function(){} ],
+//       }
+//       emit: function(eventString);
+//       //might need to store activity ids we are including 
+//    },
+//    ROUTE: {
+//       addLocation: function(location, after){},
+//       deleteLocation: function(location){}, //maybe location.delete()??
+//       moveLocation: function(location, after), //maybe location.move()??
+//       ,//potentially invert direction
+//       ,//set options (e.g. avoid, search radius, transport type)
+//       ,//route array > has events
+//       ,// options object > has events 
+//    },
+//    MAP: {
+//       ,//
+//    },
+//    RECREATION: {
+//       ,//filteredSuggestions >has events
+//       ,//bookmarks
+//       ,//bookmark function
+//       ,//inRoute
+//       ,//add to route function
+//       ,//initialLoad > has event
+//       ,//refreshNeeded > has event
+//       ,//loadSuggestions / apiCall function (on button click)
+//       ,//setAPIrequest < adds a function for a single api request 
+//       ,//setLeg/location (A to B; just A; B to C??)
+//    },
+//    on: function(eventString, callback){},
+//    events: {
+//       ready: [ function(){}, function(){} ],
+//    }
+//    emit: function(eventString),
+//    //(checks local storage and updates data appropriately)
+//    init: function(){},
+// }
+
