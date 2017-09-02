8fc92323e9e36ea6599c9a1bb2a0869ac4e00041
Trevor Nelson <tsnelson86@gmail.com>
2017-09-02 17:32:28 -0600

    phase 2 of destinations complete

--- src/components/route/route.js
+++ src/components/route/route.js
@@ -1,47 +1,53 @@
 import './route.css';
 import state from '../state/state';
 
-var startingPlace = $("<input>");
-$("#destinations").append(startingPlace);
-startingPlace.addClass("destination-input");
-startingPlace.attr("placeholder", "Starting Location: ");
-startingPlace.attr("id", "starting-place");
+var stopcount = 0;
 
-// Get the HTML input element for the autocompelte search box and create the autocomplete object
-var startingGeocoder;
-function initialize() {
-	var startingInput = $("#starting-place")[0];
-	var startingAutocomplete = new google.maps.places.Autocomplete(startingInput);
-	startingGeocoder = new google.maps.Geocoder();
+newInputField();
+
+var options = {
+  componentRestrictions: {country: 'us'}
+};
+
+// Applied autofill code to the new input fields
+function autofill(input){
+	var autocomplete = new google.maps.places.Autocomplete(input, options);
+	autocomplete.addListener('place_changed', function () {getAddress(autocomplete);});
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// Return values to state object
+function getAddress(autocomplete) {
+	var place = autocomplete.getPlace();
+	console.log(place);
+	console.log(place.geometry.location.lat());
+	console.log(place.geometry.location.lng());
+	$("#destinations").append("<div id='newbuttons'>");
+	$("#newbuttons").append("<a class='btn-floating btn-small waves-effect waves-light red' id='route-addBtn'><i class='material-icons'>add</i></a>");
+	$("#newbuttons").append("<p id='route-newLocationText'>Add a New Stop</p>");
+	$("#route-addBtn").click(newInputField);
+}
 
-
-//id="destinations"
-//<input type="text" id="starting-place" class="Autocomplete" placeholder="Starting Place:">
-//<input type="text" id="place-a" class= "Autocomplete" placeholder="Point A:">
-//<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+// Get the HTML input element for the autocompelte search box and create the autocomplete object
+// Translates address to lat/long coordinates for using on the map
+function newInputField() {
+	$("#newbuttons").remove();	
+	var inputfield = $("<input>");
+	$("#destinations").append(inputfield);
+	inputfield.addClass("destination-input");
+	inputfield.attr("id", "stopnumber" + stopcount);
+	if (stopcount == 0) {
+		inputfield.attr("placeholder", "Starting Location: ");
+	}
+	else {
+		inputfield.attr("placeholder", "Next Stop: ");
+	}
+	autofill(inputfield[0]);
+	stopcount++;
+}
 
 // create event listener for path in state object.
 // what is path?
 //    an array of location objects
 // need to fill state -> path with the name and address of 
 
-// for new users (path is empty), add starting point and point a to the input options
-// limit default search section to united states
-
 // for returning users (where path is filled), pre-fill previous route options to the input fields
-// 
-
-// c
-
-// 
-
-// 
-
-// 
-
-// 
-
-// 
