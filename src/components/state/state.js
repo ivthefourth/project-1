828d61e6d0332b56497390a85219c2e3feadea05
Howard Shaw <chip.shaw4@gmail.com>
2017-09-01 21:48:32 -0600

    recreationcollection

--- src/components/state/state.js
+++ src/components/state/state.js
@@ -38,7 +38,7 @@ class EventObject{
 
    //provides event object for event listeners; should be overwritten by inheritor
    makeEvent(){
-      console.warn(`No makeEvent method set on ${this}.`)
+      console.warn(`No makeEvent method set on ${this}`);
    }
 }
 
@@ -112,6 +112,9 @@ class Route{
    constructor(){
       ;
    }
+   addLocation(){
+      ;
+   }
    toString(){
       return 'state.route';
    }
@@ -132,18 +135,142 @@ class Map{
 /**************\    
    Recreation    
 \**************/
-class Bookmarks{
+const requiredProps = [
+   'RecAreaName',
+   'RecAreaPhone',
+   'LINK'
+];
+
+class RecArea extends EventObject{
+   constructor(area){
+      super(['change']);
+      this.id = area.RecAreaID;
+      this.activities = area.ACTIVITY.map(function(a){ 
+         return a.ActivityID; 
+      });
+      requiredProps.forEach(function(prop){
+         this[prop] = area[prop];
+      }.bind(this));
+
+      this.bookmarked = false;
+      this.inRoute = false;
+      this.focused = false;
+
+   }
+//togglebookmark> change
+//toggleonroute> change
+//setFocus > change
+}
+
+class RecAreaCollection extends EventObject{
+   constructor(name){
+      super(['change']);
+      this.name = name;
+
+      //array of "RecArea"s 
+      this.RECDATA = [];
+
+      //hash map like storage of which rec areas are currently 
+      //in this collection (by id)
+      this.idMap = {};
+   }
+   addData(recdata){
+      let change = false;
+      if( !(recdata instanceof Array)){
+         recdata = [recdata];
+      }
+      recdata.forEach(function(area){
+         if(!this.idMap[area.id]){
+            change = true;
+            this.RECDATA.push(area);
+            this.idMap[area.id] = true;
+         }
+      }.bind(this));
+      if(change){
+         this.emit('change');
+      }
+   }
+   removeData(recdata){
+      if( !(recdata instanceof Array)){
+         recdata = [recdata];
+      }
+      //only emit if data has changed... i.e., not all duplicates 
+   }
+   setData(recdata){
+      this.idMap = {};
+      if( !(recdata instanceof Array)){
+         recdata = [recdata];
+      }
+      recdata.forEach(function(area){
+         this.RECDATA.push(area);
+         this.idMap[area.id] = true;
+      }.bind(this));
+      this.emit('change');
+   }
+   makeEvent(){
+      return {val: this.RECDATA}
+   }
+   toString(){
+      return `state.recreation.${this.name}`;
+   }
+}
+
+class RecStatus extends EventObject{
    constructor(){
-      ;
+      super(['change', 'percent']);
+      this.loading = false;
+      this.percentLoaded = 100;
+      this.shouldLoad = true;
+      this.firstLoad = true;
+   }
+   update({loading, percentLoaded, shouldLoad, firstLoad} = {}){
+      let change = false;
+      if(loading !== undefined){
+         this.loading = loading;
+         change = true;
+      }
+      if(shouldLoad !== undefined){
+         this.shouldLoad = shouldLoad;
+         change = true;
+      }
+      if(firstLoad !== undefined){
+         this.firstLoad = firstLoad;
+         change = true;
+      }
+      if(change){
+         this.emit('change');
+      }
+      if(percentLoaded !== undefined){
+         this.percentLoaded = percentLoaded;
+         this.emit('percent');
+      }
+   }
+   toString(){
+      return 'state.recreation.status';
    }
 }
 
 class Recreation{
    constructor(){
-      ;
+      this.all = new RecAreaCollection('all');
+      this.filtered = new RecAreaCollection('filtered');
+      this.bookmarked = new RecAreaCollection('bookmarked');
+      this.addedToRoute = new RecAreaCollection('addedToRoute');
+
+      //temporary
+      this.all.on('change', function(e){this.filtered.setData(e.val)}.bind(this));
+
+      this.status = new RecStatus;
    }
-   addLocation(){
-      ;
+   addRecAreas(recdata){
+      var data = recdata.reduce(function(arr, area){
+         let temp = []
+         if( !this.all.idMap[area.RecAreaID] ){
+            temp.push(new RecArea(area));
+         }
+         return arr.concat(temp);
+      }.bind(this), []);
+      this.all.addData(data);
    }
    toString(){
       return 'state.recreation';
@@ -157,6 +284,7 @@ class State extends EventObject{
    constructor(){
       super(['ready']);
       this.interests = null;
+      this.recreation = new Recreation();
    }
    setInterests(list){
       this.interests = new Interests(list);
@@ -171,10 +299,8 @@ class State extends EventObject{
 
 const STATE = new State;
 
-/* REMOVE: */
-import {interestList as list} from '../recreation/recreation';
-STATE.setInterests(list);
-/*         */
+/* TEMPORARY, REMOVE LATER */
+window.state = STATE;
 
 export default STATE;
 
@@ -227,7 +353,7 @@ export default STATE;
 //       ,//inRoute
 //       ,//add to route function
 //       ,//initialLoad > has event
-//       ,//refreshNeeded > has event
+//       ,//refreshNeeded > has event.... should load more/has loaded???
 //       ,//loadSuggestions / apiCall function (on button click)
 //       ,//setAPIrequest < adds a function for a single api request 
 //       ,//setLeg/location (A to B; just A; B to C??)
