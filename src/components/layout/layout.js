b3d55f421a406655ca01f02d1084db79aae9f7ae
Jon Caviness <Jon@Jons-Mac.local>
2017-09-02 12:25:22 -0600

    adding changes

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,5 +2,12 @@ import './layout.css';
 import state from '../state/state';
 
 export default function(){ 
-   $('body').append( $('<h2 class="layout">').text('layout') )
+   $('body').append( $('<h2 class="layout">').text('layout') );
 }
+
+$(document).ready(function() {
+    $('select').material_select();
+	for (var i = 0; i < interestList.length; i++) {
+		$("#interests").append(state.interestList[i]);
+	}
+  });
