2449b7342f6634187c1af3435255d703dc3d90fb
Howard Shaw <chip.shaw4@gmail.com>
2017-09-02 14:12:07 -0600

    semicolon

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import './components/recreation/recreation'
+import './components/recreation/recreation';
 import './components/interests/interests';
 import './components/layout/layout';
 import './components/map/map';
