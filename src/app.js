336873d896b544d13f8089af4ed25b44c90f4855
Trevor Nelson <tsnelson86@gmail.com>
2017-09-02 14:03:55 -0600

    adding mapping api - functioning code

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@ import './components/recreation/recreation'
 import interests from './components/interests/interests';
 import layout from './components/layout/layout';
 import map from './components/map/map';
-import route from './components/route/route';
+import './components/route/route';
 
 interests();
 layout();
 map();
-route();
 
