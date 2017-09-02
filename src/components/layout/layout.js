53d2a1c1858199a94fe5cbc27f6a5dd2790ac62c
Jon Caviness <Jon@Jons-Mac.local>
2017-09-02 14:12:40 -0600

    update

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -6,7 +6,26 @@ export default function(){
 }
 
 $(document).ready(function() {
+    // makes the select options work
     $('select').material_select();
+
+    // makes the modal work, and adds options
+
+
+	$('.modal').modal({
+  		dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    	opacity: .5, // Opacity of modal background
+    	inDuration: 300, // Transition in duration
+    	outDuration: 200, // Transition out duration
+    	startingTop: '4%', // Starting top style attribute
+    	endingTop: '10%', // Ending top style attribute
+    	ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      	console.log(modal, trigger);
+   	},
+   
+  	}
+	);
+      
     
 	for (let i = 0; i < state.interests.all.length; i++) {
 		let newChip = $('<div class="chip"><a href=""></a></div>');
