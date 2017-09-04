810ef236cf9747823d83298fab8e84d1ad89c102
Elizabeth Nguyen <2krazykats@gmail.com>
2017-09-05 00:49:41 +0100

    changes to merge with changes in master

---
+++ src/components/recreation/recAreaDetails.js
@@ -0,0 +1,14 @@
+/* Retrieve the data for a recreation area based on RecAreaID
+*  Display the data to a modal on the web page */
+
+
+var recAreaId;
+
+export function retrieveRecAreaById(recAreaId) {
+
+    // retrieve the data using recAreaId
+
+
+    // display the data in a modal box
+
+}
