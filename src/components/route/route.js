c0ba9ac8de439b39bff3ebc3c8ca2c7804686266
Trevor Nelson <tsnelson86@gmail.com>
2017-09-05 20:22:16 -0600

    Added ability to create multiple destinations without adding more + buttons, stored all values in state, and removed values from state if the field is blank

--- src/components/route/route.js
+++ src/components/route/route.js
@@ -3,28 +3,50 @@ import state from '../state/state';
 
 var stopcount = 0;
 
-newInputField();
-
 var options = {
   componentRestrictions: {country: 'us'}
 };
 
-// Applied autofill code to the new input fields
-function autofill(input){
-	var autocomplete = new google.maps.places.Autocomplete(input, options);
-	autocomplete.addListener('place_changed', function () {getAddress(autocomplete);});
-}
+newInputField();
 
-// Return values to state object
-function getAddress(autocomplete) {
-	var place = autocomplete.getPlace();
-	console.log(place);
-	console.log(place.geometry.location.lat());
-	console.log(place.geometry.location.lng());
+state.route.on("change", function (e){
+	var path = e.val;
+	$("#destinations").empty();
+	if (path.length == 0) {
+		newInputField();
+	} else {
+		for (let i = 0; i < e.val.length; i++) {
+			var location = e.val[i];
+			let newInput = $("<input>").val(location.data.name + ' (' + location.data.formatted_address + ')');
+			newInput.focusout(function(){
+				if (newInput.val() == ""){
+					state.route.remove(i);
+				}
+			});
+			$("#destinations").append(newInput);
+			autofill(newInput[0], false, i);
+			$("#destinations").append("<br>");
+		} 
+	}
 	$("#destinations").append("<div id='newbuttons'>");
 	$("#newbuttons").append("<a class='btn-floating btn-small waves-effect waves-light red' id='route-addBtn'><i class='material-icons'>add</i></a>");
 	$("#newbuttons").append("<p id='route-newLocationText'>Add a New Stop</p>");
 	$("#route-addBtn").click(newInputField);
+});
+
+// Applied autofill code to the new input fields and sends input to state object
+function autofill(input, add, index){
+	var autocomplete = new google.maps.places.Autocomplete(input, options);
+	autocomplete.addListener('place_changed', function (){
+		var place = autocomplete.getPlace();
+		if (add){
+			state.route.add(place);
+		}
+		else {
+			state.route.remove(index);
+			state.route.insert(place, index);
+		}
+	});
 }
 
 // Get the HTML input element for the autocompelte search box and create the autocomplete object
@@ -34,14 +56,13 @@ function newInputField() {
 	var inputfield = $("<input>");
 	$("#destinations").append(inputfield);
 	inputfield.addClass("destination-input");
-	inputfield.attr("id", "stopnumber" + stopcount);
 	if (stopcount == 0) {
 		inputfield.attr("placeholder", "Starting Location: ");
 	}
 	else {
 		inputfield.attr("placeholder", "Next Stop: ");
 	}
-	autofill(inputfield[0]);
+	autofill(inputfield[0], true);
 	stopcount++;
 }
 
