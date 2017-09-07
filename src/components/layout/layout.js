11a812437e3af2799c7767f740af60fe7293da2a
Jon Caviness <Jon@Jons-Mac.local>
2017-09-06 19:52:18 -0600

    added local storage for interests

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,31 +4,51 @@ import state from '../state/state';
 $(document).ready(function() {
     $('select').material_select();
     
-	for (let i = 0; i < state.interests.all.length; i++) {
-		let newChip = $('<div class="chip"></div>');
-		$("#interests").append(newChip.text(state.interests.all[i].name));
-		$(newChip).click(function() {
-			state.interests.all[i].toggle();
-		});
-	state.interests.all[i].on('change', function(e) {
-		console.log(e);
-		if(e.val) {
-			newChip.addClass("selected");
-			$("#selected-interests").append(newChip);
-		} else {
-		 	newChip.removeClass('selected');
-		 	$("#unselected-interests").prepend(newChip);
-		}
+	
+    function addChip() {
+		for (let i = 0; i < state.interests.all.length; i++) {
+			
+			let newChip = $('<div class="chip"></div>');
+			$("#unselected-interests").append(newChip.text(state.interests.all[i].name));
+			
+			$(newChip).click(function() {
+				state.interests.all[i].toggle();
+			});
 
-	});
+		state.interests.all[i].on('change', function(e) {
+			if(e.val) {
+				newChip.addClass("selected");
+				$("#selected-interests").append(newChip);
+			} else {
+			 	newChip.removeClass('selected');
+			 	$("#unselected-interests").prepend(newChip);
+			}
+
+		});
+		}
 	}
 
-	var selectedInterestsArray = [];
+	addChip();
 
-	$(".chip").click(function(){
-		console.log($(this).html());
-		selectedInterestsArray.push($(this).html());
-		console.log(selectedInterestsArray);
-		localStorage.setItem('selectedInterestsArray', selectedInterestsArray);
+	state.interests.on('change', function(e) {
+		var interests = {};
+		console.log(e.val.selected);
+
+		e.val.selected.forEach(function(interest) {
+			interests[interest.id] = true;
+		});
+		console.log(interests);
+		localStorage.setItem('interests', JSON.stringify(interests));
 	});
+
+	// $("#clear-interests").click(function() {
+	
+	// 	state.interests.all.forEach(function(clear) {
+	// 		clear.selected = false;
+	// 	});
+	// 	state.interests.emit('change');
+	// 	console.log(state);
+	// });
 });
+
+
